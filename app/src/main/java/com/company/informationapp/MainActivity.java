f1a7b65b875685490d3735ab3807a65fd6bfb6e2
yashar-akish <yashar_akish@yahoo.com>
2021-05-18 04:56:50 -0400

    creating the objects of the Modal

--- app/src/main/java/com/company/informationapp/MainActivity.java
+++ app/src/main/java/com/company/informationapp/MainActivity.java
@@ -8,9 +8,14 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
+import com.company.informationapp.model.ModelClass;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private ArrayList<ModelClass> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         //recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         //recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        arrayList = new ArrayList<>();
+
+        ModelClass modelClass1 = new ModelClass("planets", "The Planets");
+        ModelClass modelClass2 = new ModelClass("music", "Classic musician");
+        ModelClass modelClass3 = new ModelClass("usa", "The USA presidents");
+        ModelClass modelClass4 = new ModelClass("worldmap", "The Countries");
+
+        arrayList.add(modelClass1);
+        arrayList.add(modelClass2);
+        arrayList.add(modelClass3);
+        arrayList.add(modelClass4);
 
     }
 }
