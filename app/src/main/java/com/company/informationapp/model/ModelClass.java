a88c025abc32ca19477a16f92139dcb9f5de2d65
yashar-akish <yashar_akish@yahoo.com>
2021-05-18 04:39:44 -0400

    creating the Model

---
+++ app/src/main/java/com/company/informationapp/model/ModelClass.java
@@ -0,0 +1,31 @@
+package com.company.informationapp.model;
+
+public class ModelClass {
+
+    private String imageName;
+    private String categoryName;
+
+    public ModelClass() {
+    }
+
+    public ModelClass(String imageName, String categoryName) {
+        this.imageName = imageName;
+        this.categoryName = categoryName;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
